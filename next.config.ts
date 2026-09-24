307ae4e46e27a4b7effeeb6500365f5f8994157c
Donovan Bueno <134464142+DevDonov4n@users.noreply.github.com>
2026-09-24 10:22:14 -0300

    fix: permitir imagens da Morena no next image

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
         hostname: "res.cloudinary.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "www.morenalingerie.com.br",
+        pathname: "/tags/**",
+      },
     ],
   },
 };
